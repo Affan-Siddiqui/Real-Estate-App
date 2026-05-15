bbd25acc8e47ddee11e71642d1d440895702a9ac
Affan-Siddiqui <m.affans913@gmail.com>
2026-05-15 21:27:03 +0500

    chore: update dependencies and migrate to Prisma 5.11.0
    - Added @prisma/client and updated prisma to version 5.11.0 in package.json
    - Introduced new dependencies: cookie-parser, dotenv, mongodb
    - Added devDependencies for TypeScript support
    - Removed outdated prisma.config.ts file
    - Updated schema.prisma to use the new client provider and MongoDB datasource

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,22 +1,56 @@
 import bcrypt from 'bcrypt';
+import prisma from "../lib/prisma.js";
+
 
 export const register = async (req, res) => {
 
-    const { username, email, password} = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    res.body = {
-        username : username,
-        email : email,
-        password : hashedPassword
+    try {
+      const { username, email, password} = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const newUser = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+  
+      console.log(newUser);
+      
+      res.status(201).json({message : "User Created Successfully."})    
+    } catch (error) {
+        console.log(error);
+        res.status(599).json({message : "Failed to create user"});
     }
-
-    console.log(res.body);
-    
 }
 
-export const login = (req, res) => {
-    console.log("login end point");
+export const login = async (req, res) => {
+    
+    try {
+      const { username, password} = req.body;
+
+      const user = await prisma.user.findUnique({
+        where : {username}
+      })
+      if (!user) return res.status(401).json({message : "Invalid Credentials"})
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) return res.status(401).json({message : "Incorrect Password"});
+      
+      // res.setHeader("Set-Cookie", "test =" + "myvalue" ).json({message : "Login succesfully !"})
+
+      const age = 1000 * 5
+
+      res.cookie("test2", "my-value2", {
+        httpOnly : true,
+        maxAge : age
+      }).status(200).json({"message" : "Login Successfully !"});
+
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({message : "Failed to Login"});
+    }
 }
 
 export const signout = (req, res) => {
--- api/index.js
+++ api/index.js
@@ -3,9 +3,13 @@ import  express  from "express";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import userRoute from "./routes/user.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
 app.use(express.json());
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
